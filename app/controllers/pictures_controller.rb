06468bb9974eb6ff205b991967742fa765e3a583
Kenji <fcb141021@yahoo.co.jp>
2023-08-13 16:53:29 +0900

    一覧表示の追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,24 @@
 class PicturesController < ApplicationController
   def index
+    @Pictures = Picture.all
+  end
+
+  def new
+    @pictures = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to new_picture_path
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image, :content)
   end
 end
